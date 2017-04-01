1e189773002582dadf8b1c5aee838b6f05357ec4
Philipp Rosenkranz <ph.rosenkranz@gmail.com>
2017-04-02 01:37:38 +0200

    Changed parts of the API to pass by value (rest will be fixed soon)

--- uint128.go
+++ uint128.go
@@ -21,7 +21,7 @@ type Uint128 struct {
 	H, L uint64
 }
 
-func (u *Uint128) Compare(o *Uint128) int {
+func (u *Uint128) Compare(o Uint128) int {
 	if u.H < o.H {
 		return lessThan
 	} else if u.H > o.H {
@@ -37,68 +37,71 @@ func (u *Uint128) Compare(o *Uint128) int {
 	return equal
 }
 
-func (u *Uint128) And(o *Uint128) {
+func (u *Uint128) And(o Uint128) {
 	u.H &= o.H
 	u.L &= o.L
 }
 
-func (u *Uint128) Or(o *Uint128) {
+func (u *Uint128) Or(o Uint128) {
 	u.H |= o.H
 	u.L |= u.L
 }
 
-func (u *Uint128) Xor(o *Uint128) {
+func (u *Uint128) Xor(o Uint128) {
 	u.H ^= o.H
 	u.L ^= o.L
 }
 
 // See: https://www.codeproject.com/Tips/617214/UInt-Addition-Subtraction
 // for an explanation
-func (u *Uint128) Add(o *Uint128) {
+func (u *Uint128) Add(o Uint128) (ans Uint128) {
 	var C uint64 = (((u.L & o.L) & 1) + (u.L >> 1) + (o.L >> 1)) >> 63
 	u.H += o.H + C
 	u.L = u.L + o.L
+	ans.H = u.H
+	ans.L = u.L
+	return ans
 }
 
 // See: https://www.codeproject.com/Tips/617214/UInt-Addition-Subtraction
 // for an explanation
-func (u *Uint128) Sub(o *Uint128) {
+func (u *Uint128) Sub(o Uint128) (ans Uint128) {
 	u.L = u.L - o.L
 	var C uint64 = (((u.L & o.L) & 1) + (o.L >> 1) + (u.L >> 1)) >> 63
 	u.H -= o.H + C
+	ans.H = u.H
+	ans.L = u.L
+	return
 }
 
-func NewFromUint64(uint64 uint64) *Uint128 {
-	u := new(Uint128)
-	u.L = uint64
-	u.H = 0
-	return u
+func NewFromUint64(uint64 uint64) (ans Uint128) {
+	ans.L = uint64
+	ans.H = 0
+	return
 }
 
-func NewFromString(s string) (u *Uint128, err error) {
+func NewFromString(s string) (u Uint128, err error) {
 
 	if len(s) > Len {
-		return nil, fmt.Errorf("s:%s length greater than 32", s)
+		return NewFromUint64(0), fmt.Errorf("s:%s length greater than 32", s)
 	}
 
 	b, err := hex.DecodeString(fmt.Sprintf("%032s", s))
 	if err != nil {
-		return nil, err
+		return NewFromUint64(0), err
 	}
 	rdr := bytes.NewReader(b)
-	u = new(Uint128)
-	err = binary.Read(rdr, binary.BigEndian, u)
+	err = binary.Read(rdr, binary.BigEndian, &u)
 	return
 }
 
-func NewFromBigEndianBytes(b []byte) (u *Uint128, err error) {
+func NewFromBigEndianBytes(b []byte) (u Uint128, err error) {
 	if len(b) > LenBytes {
-		return nil, fmt.Errorf("length greater than 16 bytes")
+		return NewFromUint64(0), fmt.Errorf("length greater than 16 bytes")
 	}
 	rdr := bytes.NewReader(b)
-	u = new(Uint128)
-	err = binary.Read(rdr, binary.BigEndian, u)
-	return
+	err = binary.Read(rdr, binary.BigEndian, &u)
+	return u, err
 }
 
 func (u *Uint128) BigEndianBytes() []byte {
